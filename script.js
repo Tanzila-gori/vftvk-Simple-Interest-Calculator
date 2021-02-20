9c0ea272560a34120f17853f069f7b1c9df61bb9
Tanzila-gori <66572653+Tanzila-gori@users.noreply.github.com>
2021-02-21 03:14:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var y = document.getElementById("years").value;
+    var interest = document.getElementById('result').value="principal * years * rate / 100";
+    document.getElementById("result").innerHTML = " + principal * years * rate / 100;
 }
-        
+var year = currentTime.getFullYear()
+  function myFunction() {
+  var x = document.getElementById("rate").value;
+  document.getElementById("rates").innerHTML = " " + x;
+}      
+
+     
